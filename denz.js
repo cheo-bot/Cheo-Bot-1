593dceb5389c0c86fa551974920c8bc4be3a87d0
Ghoskyy <91611351+Ghoskyy@users.noreply.github.com>
2021-10-25 11:11:44 +0700

    Update denz.js

--- denz.js
+++ denz.js
@@ -1108,7 +1108,7 @@ sendEphemeral: false,
 "mediaType": "2",
 "mediaUrl": "https://youtu.be/JN_Gw2GzuqQ",
 "thumbnail": pporigi,
-},mentionedJid:[stod])
+,mentionedJid:[stod]}}})
  if(menusimpel = true){
         stod = `${sender}`
        stst = await denz.getStatus(`${sender.split('@')[0]}@c.us`)
